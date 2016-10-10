58a4ca730cacea3391b819bc3ca069e2207b52d4
JBV <bouviervj@gmail.com>
2016-10-10 06:13:10 -0400

    Removed artefact from crashed arm asset

---
+++ Hololens/HololensFromScratch/Assets/RemoveArm.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveArm : MonoBehaviour {
+
+    public Vector3 scale;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void LateUpdate()
+    {
+        transform.localScale = scale;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
